5dae3c3aed5201647e08fd38ec12650c2c1649f9
qiaoyuan <34348492@qq.com>
2024-08-21 10:59:47 +0800

    update

--- config/domain.php
+++ config/domain.php
@@ -3,10 +3,10 @@
 return [
     'm' => env('domain.m', 'www'),
     'www' => env('domain.www', 'www'),
-    'image' => env('domain.image', ''),
+    'image' => env('domain.image', 'psychology'),
     'static' => env('domain.static', ''),
     'admin' => env('domain.admin', ''),
-    'api' => env('domain.api', ''),
-    'root' => env('domain.root', 'psychology.xuanzeti.top'),
+    'api' => env('domain.api', 'psychology'),
+    'root' => env('domain.root', '.xuanzeti.top'),
     'schema' => env('domain.schema', 'https://'),
 ];
