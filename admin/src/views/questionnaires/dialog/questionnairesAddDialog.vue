e2ec5eb4ff6dcbb07afe22844a42446c2dd67a4a
LZL <1441596783@qq.com>
2024-09-02 11:14:59 +0800

    update

--- admin/src/views/questionnaires/dialog/questionnairesAddDialog.vue
+++ admin/src/views/questionnaires/dialog/questionnairesAddDialog.vue
@@ -6,7 +6,30 @@
     :action="formAction"
     :width="$w_fun.isMobile() ? '100%' : '60%'"
     @done="v => $emit('done', v)"
-  />
+  >
+    <template #phaseSlot="{item, model, options }">
+       <div  v-for="(item,i) in model.group_conf" :key="i" class="selectContent">
+        <el-row >
+          <el-col :span="2">
+            第{{i+1}} 阶段
+          </el-col>
+          <el-col :span="9">
+            <el-form-item :label="'首个题目'" prop="start" label-width="80px">
+              <el-input  v-model="item.start" clearable placeholder="输入首个题目" type="number"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="9">
+            <el-form-item label="末尾题目" prop="end" label-width="80px">
+                <el-input v-model="item.end" clearable placeholder="输入题目" type="number"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="1"><el-button circle icon="el-icon-plus" @click="addList()"></el-button></el-col>
+          <el-col :span="1"><el-button circle icon="el-icon-minus" @click="subList(i)" v-if="i>0"></el-button></el-col>
+        </el-row>
+    </div>
+    <!-- <div v-else> 全部阶段</div> -->
+    </template>
+  </w-dialog-form>
 </template>
 
 <script>
@@ -17,11 +40,14 @@ export default {
       module: 'questionnaires',
       formAction: '',
       form: {},
-      readonly: false
+      readonly: false,
+      studentList: []
     }
   },
   methods: {
-    setForm({ id, title, description, status, img_url, article_category_id, easy, exact, utility, sort,price,content }) {
+    setForm({ id, title, description, status, img_url, article_category_id, easy, exact, utility, sort,price,content, group_index, group_conf }) {
+      this.studentList = group_conf
+      console.log(group_conf)
       this.form = {
         title: { label: '问卷名称', value: title },
         description: { label: '问卷简述', value: description, required: false },
@@ -36,6 +62,21 @@ export default {
         easy: {  width: '50%', label: '题目易懂', value: easy },
         exact: {   width: '50%', label: '结果准确性', value: exact },
         utility: {  width: '50%', label: '建议实用性', value: utility },
+        // group_index: {
+        //   label: '是否分配阶段',
+        //   value: group_index,
+        //   formType: 'select',
+        //   options: [{
+        //     label: '是',value: 1
+        //   },{
+        //     label: '否',
+        //     value: 0
+        //   }],
+        //   input: (val) => {
+        //     console.log(val)
+        //   }
+        // },
+        group_conf: { label: '评分阶段',value: group_conf, formType: 'phaseSlot', required: false },
         sort: { label: '排序', value: sort},
         content: { label: '内容', value: content, formType: 'editor'},
       }
@@ -53,6 +94,14 @@ export default {
     },
     getForm() {
       return this.$refs.wDialogForm.$refs.form
+    },
+    // 加号
+     addList() {
+      this.studentList.push({title: '', score: '',sort: ''})
+    },
+    //减号
+    subList(index) {
+      this.studentList.splice(index, 1)
     }
   }
 }
