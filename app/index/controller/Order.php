e86ac75c6f6882b4de3b93eadd2ae8fbbf328e6d
qiaoyuan <34348492@qq.com>
2024-09-14 17:44:54 +0800

    greng

--- app/index/controller/Order.php
+++ app/index/controller/Order.php
@@ -8,9 +8,18 @@ use app\common\model\QuestionnairesOrderCallback;
 use app\common\model\Snowflake;
 use app\index\BaseController;
 use EasyWeChat\Factory;
+use GuzzleHttp\Client;
 
 class Order extends BaseController
 {
+    public $client;
+    public function __construct()
+    {
+        $this->client = new Client([
+            'timeout' => 30,
+            'base_uri' => 'https://developer.toutiao.com/'
+        ]);
+    }
 
     public function create()
     {
@@ -22,7 +31,7 @@ class Order extends BaseController
             $this->error('问卷不存在');
         }
 
-        if (!empty($param['order_timeout'])){
+        if (!empty($param['order_timeout'])) {
             $order = QuestionnairesOrder::where('order_id', $param['order_timeout'])->find();
             $order->status = 0;
             $order->remark = '订单超时.';
@@ -32,8 +41,6 @@ class Order extends BaseController
 
             $order = QuestionnairesOrder::where(['uid' => $this->getUid(), 'questionnaire_id' => $param['questionnaire_id'], 'status' => 1])->find();
         }
-
-
 
 
         if (!empty($order) && $order->price != $questionnairesObj->price) {
@@ -100,14 +107,14 @@ class Order extends BaseController
 
             \Yansongda\Pay\Pay::config($config);
             $orderInput = [
-                'out_order_no' => $order->order_id.'',
+                'out_order_no' => $order->order_id . '',
                 'total_amount' => $questionnairesObj->price * 100,
                 'subject' => $questionnairesObj->title,
                 'body' => "qy - test - body - 01",
                 'valid_time' => 600,
             ];
 
-            $result =  \Yansongda\Pay\Pay::douyin()->mini($orderInput);
+            $result = \Yansongda\Pay\Pay::douyin()->mini($orderInput);
             $order->pay_extent = json_encode($result);
             $order->save();
 
@@ -174,7 +181,7 @@ class Order extends BaseController
     {
         $input['head'] = $this->request->header();
         $input['param'] = $this->request->param();
-        QuestionnairesOrderCallback::create(['input_data' => json_encode($input), 'created_at' => time(), 'platform'=>1]);
+        QuestionnairesOrderCallback::create(['input_data' => json_encode($input), 'created_at' => time(), 'platform' => 1]);
 
 
         $config = [
@@ -217,13 +224,12 @@ class Order extends BaseController
     }
 
 
-
     //回调支付
     public function dycallback()
     {
         $input['head'] = $this->request->header();
         $input['param'] = $this->request->param();
-        $calObj = QuestionnairesOrderCallback::create(['input_data' => json_encode($input), 'created_at' => time(), 'platform'=>2]);
+        $calObj = QuestionnairesOrderCallback::create(['input_data' => json_encode($input), 'created_at' => time(), 'platform' => 2]);
 
         $config = [
             'douyin' => [
@@ -257,19 +263,24 @@ class Order extends BaseController
         $result = \Yansongda\Pay\Pay::douyin()->callback();
         $calObj->res = json_encode($result);
         $calObj->save();
-        if($result['type'] == 'payment') {
+        if ($result['type'] == 'payment') {
             $orderRes = json_decode($result['msg'], true);
             $orderObj = QuestionnairesOrder::where('order_id', $orderRes['cp_orderno'])->find();
             $orderObj->pay_status = QuestionnairesOrder::PAY_PAID_STATUS;
 
-            if($orderRes['way'] == "1") {
+            if ($orderRes['way'] == "1") {
                 $orderObj->pay_type = QuestionnairesOrder::PAY_TYPE_DY_WX;
-            } else if($orderRes['way'] == "2") {
+            } else if ($orderRes['way'] == "2") {
                 $orderObj->pay_type = QuestionnairesOrder::PAY_TYPE_DY_ALIPAY;
             } else {
                 $orderObj->pay_type = QuestionnairesOrder::PAY_TYPE_DY_PAY;
             }
             $orderObj->save();
+
+            $order = $orderObj->toArray();
+            $goods = Questionnaires::where('id', $orderObj->questionnaire_id)->find()->toArray();
+            $openId =  \app\common\model\User::find($order['uid'])->open_id;
+            $this->pushOrder($goods, $order, $openId);
 
             $answer = QuestionAnswer::where('uid', $orderObj->uid)->where('questionnaire_id', $orderObj->questionnaire_id)->find();
             if (empty($answer)) {
@@ -286,6 +297,79 @@ class Order extends BaseController
 
         return \Yansongda\Pay\Pay::douyin()->success();
 
+    }
+
+    /**
+     * 订单推送到抖音
+     * @param $data array 订单数据
+     * @note order_status 与 status须保持一致,但类型不同
+     * @return array
+     */
+    public function pushOrder($goods,$order,$openId)
+    {
+        $data = [];//获取订单信息
+        $api = "api/apps/order/v2/push";
+        $openid = '';//获取下单用户openid
+        //组装商品
+        $item_list[] = [
+            'item_code' => '1001'.$goods['id'],
+            'img' => $goods['img_url'],
+            'title' => $goods['title'],
+            'amount' => 1,
+            'price' => (int)($goods['price'] * 100),
+
+        ];//参数对应请查看官方文档，注意字段类型
+        // 组装订单
+        $orderDetail = [
+            'order_id' => $order['order_id'].'', 'create_time' => strtotime($order['created_at']), 'status' => '已支付', 'amount' => 1,
+            'total_price' => (int)($order['price'] * 100), 'detail_url' => "pages/order/orderDetail", 'item_list' => $item_list];
+
+        //{\"detail_url\":\"https://www.xxxx.com/shop/order/orderDetail?orderId=21000240218164635217330&pad_check=df126473398e4840111ba0c620ca1c5c\",\"amount\":2,\"create_time\":1708245997095,\"total_price\":2,\"item_list\":[{\"amount\":1,\"img\":\"https://www.xxxx.com/resources/2063/1915501.jpg\",\"price\":1,\"title\":\"字节小程序语音版\"},{\"amount\":1,\"img\":\"https://www.xxxx.com/resources/2063/19155_01.jpg\",\"price\":1,\"title\":\"主卡\"}],\"order_id\":\"21000240218164635217330\",\"status\":\"订单已完成\"}
+        $param = ['access_token' => $this->getAccessTokens(), 'app_name' => "douyin",
+            'open_id' => $openId, 'update_time' => $this->getMillisecond(), 'order_detail' => json_encode($orderDetail,JSON_UNESCAPED_UNICODE), 'order_type' => 0, 'order_status' => 1];
+
+
+        $body = $this->client->post($api, ['json' => $param]);
+        $result = json_decode($body->getBody()->getContents(), true);
+
+        if(isset($result['err_code']) && $result['err_code'] === 0) {
+           return 0;
+        }
+        return  -1;
+
+
+
+    }
+
+    /**
+     * 获取AccessToken
+     */
+    public function getAccessTokens()
+    {
+        $api = "api/apps/v2/token";
+        $param = ['appid' => config('douyin.mini_app_id'), 'secret' => config('douyin.secret'), 'grant_type' => "client_credential"];
+        $body = $this->client->post($api, ['json' => $param]);
+        $res = $body->getBody()->getContents();
+        $data = json_decode($res,true);
+        if ($data['err_no'] == 0) {
+            $access_token = $data['data']['access_token'];
+        }
+        return $access_token;
+    }
+
+    public function getMillisecond()
+    {
+        list($t1, $t2) = explode(' ', microtime());
+        return (float)sprintf('%.0f', (floatval($t1) + floatval($t2)) * 1000);
+    }
+
+    public function test() {
+
+        $order = QuestionnairesOrder::find(150)->toArray();
+        $goods = \app\common\model\Questionnaires::find(33)->toArray();
+        $openId =  \app\common\model\User::find($order['uid'])->open_id;
+        echo $this->pushOrder($goods, $order, $openId);
+        die;
     }
 
 }
