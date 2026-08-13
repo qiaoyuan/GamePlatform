7105348c6c7e9a05f6360f91d694ef67cbef45dc
qiaoyuan <34348492@qq.com>
2026-08-13 17:37:25 +0800

    greng

--- admin/dist/static/js/chunk-2d230fe7.347fc4b7.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230fe7"],{ef3c:function(e,r,t){"use strict";t.r(r);var n,u,c={created(){const{params:e,query:r}=this.$route,{path:t}=e;this.$router.replace({path:"/"+t,query:r})},render:function(e){return e()}},a=c,o=t("0c7c"),p=Object(o["a"])(a,n,u,!1,null,null,null);r["default"]=p.exports}}]);
